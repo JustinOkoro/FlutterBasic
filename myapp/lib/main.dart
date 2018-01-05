edfb79eaf10ce4fa7f1e225dd2db956a98669a42
Justin Okoro <justinokoro@Justins-MBP-2.home>
2018-01-05 02:56:55 -0800

    Added icons and sending button

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,25 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new FriendlyChatApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+      title: 'Chat App',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("FriendlyChat"),
+        ),
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "FriendlyChat",
+      home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textEditingController =
+      new TextEditingController();
+
+  void _handleSubmitted(String text) {
+    _textEditingController.clear();
+  }
+
+  Widget _buildTextComposer() {
+    return new Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(children: <Widget>[
+          new Flexible(
+              child: new TextField(
+            controller: _textEditingController,
+            onSubmitted: _handleSubmitted,
+            decoration:
+                new InputDecoration.collapsed(hintText: "Send a Message"),
+          )),
+          new Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                  icon: new Icon(Icons.send),
+                  onPressed: () =>
+                      _handleSubmitted(_textEditingController.text)))
+        ]));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(title: new Text("FriendlyChat")),
+      body: _buildTextComposer(),
     );
   }
 }
